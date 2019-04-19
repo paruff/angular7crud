a8f0b97426f41fc6376d3c972197e5d638a8f49e
tzerbass <tzer.bass@gmail.com>
2019-04-19 14:53:03 -0500

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GstAddComponent } from './gst-add/gst-add.component';
+import { GstEditComponent } from './gst-edit/gst-edit.component';
+import { GstGetComponent } from './gst-get/gst-get.component';
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BusinessService } from './business.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GstAddComponent,
+    GstEditComponent,
+    GstGetComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SlimLoadingBarModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    BusinessService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
